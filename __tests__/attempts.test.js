d55245f780648480f4da2e9a615d586e99ba07b7
TravelFiend <mikegrace274@gmail.com>
2019-12-11 14:54:46 -0800

    attempts routes written

---
+++ __tests__/attempts.test.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const Attempt = require('../lib/models/Attempt');
+
+describe('event routes', () => {
+    
+});
